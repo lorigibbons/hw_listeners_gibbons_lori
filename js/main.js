f37cb30a231ccc3db0b3ebed0f3ae492d883eaf8
Lori Jane Gibbons <gibbons@uga.edu>
2017-07-07 12:27:25 -0400

    Make h1Event almost work

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-var h1Event = document.getElementById( 'h1Event');
-
 h1Event.addEventListener( 'click', function(){
-    var newParagraph = document.createElement( 'paragraph');
-    newParagraph.innerHTML = '<p>this is click number x</p>'
+    var newParagraph = document.getElementById( 'h1Event');
+    newParagraph.innerHTML = 'this is click number x';
 });
