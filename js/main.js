85a55a2c5e4c7479db1dd251343f2e417276a244
Lori Jane Gibbons <gibbons@uga.edu>
2017-07-07 13:02:02 -0400

    Modified Js File

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var newParagraph = document.getElementById( 'h1Event');
 
-newParagraph.addEventListener( 'click', function(){
+newParagraph.addEventListener( 'click', function() {
     var newElement = document.createElement( 'paragraph' );
     newParagraph.innerHTML = "<p>This is click number 1</p>";
     document.getElementsByClassName( 'container' ) [0].appendChild( newElement);
