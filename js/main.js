e710a80ee39482e641c0f54c9f29b448c9cd6951
Lori Jane Gibbons <gibbons@uga.edu>
2017-07-10 14:55:16 -0400

    Add new notes to JS file, and move the numGenerator

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-//First item- change h1 color
+//First item
+//This changes the h1 color from  black to orange when moused over, and goes back to black when the mouse leaves the h1.
 var pageHeading = document.getElementById( 'heading' );
 pageHeading.addEventListener('mouseover', function(){
 	pageHeading.className =  'orange';
@@ -7,7 +8,8 @@ pageHeading.addEventListener('mouseleave', function(){
 	pageHeading.className =  '';
 });
 
-//Second item-Add text
+//Second item-
+//This adds text under the pictures when the first picture is clicked.
 var addText = document.images[0];
 addText.addEventListener('click', function(){
     var newElement = document.createElement('h3');
@@ -15,15 +17,16 @@ addText.addEventListener('click', function(){
     document.getElementsByClassName( 'clearfix')[0].appendChild( newElement );
 });
 
-//Third item-
+//Third item- this doesn't work, I am trying to remove the text that was added in the second item.
 var removeText = document.images[1];
 removeText.addEventListener('dblclick', function(){
     var newElement = document.getElementsByName('h3');
-    newElement.innerHTML = "<p>Changed</>";
+    newElement.innerHTML = "<p></p>";
     document.getElementsByName( 'clearfix' )[0].appendChild(newElement);
 });
 
-//Other item- Add random numbers - https://www.w3schools.com/js/js_htmldom_eventlistener.asp
+//Other item-
+//Adds random numbers to footer area when the mouse is moving. https://www.w3schools.com/js/js_htmldom_eventlistener.asp
 document.getElementById("numGen").addEventListener("mousemove", theFunction);
 function theFunction(){
     document.getElementById('numbers').innerHTML = Math.random();
