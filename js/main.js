78377fb3213d75d5d31e004fd57c1a322b9b8bfc
Lori Jane Gibbons <gibbons@uga.edu>
2017-07-10 13:59:24 -0400

    Add firt JS thing to change

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//First item- change h1 color
+var pageHeading = document.getElementById( 'heading' );
+pageHeading.addEventListener('mouseover', function(){
+	pageHeading.className =  'orange';
+});
+pageHeading.addEventListener('mouseleave', function(){
+	pageHeading.className =  '';
+});
+
+//Second item-
+var addText = document.getElementById( '' );
