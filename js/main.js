1bb0baf2de96325a5191c621f603a012a7b751b2
Lori Gibbons <lori.j.gibbons@gmail.com>
2017-07-11 21:03:18 -0400

    Add correct listener in class

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-var newParagraph = document.getElementById( 'h1Event');
+var newParagraph = document.getElementsByTagName('h1')[0]
 
-newParagraph.addEventListener( 'click', function() {
-    var newElement = document.createElement( 'paragraph' );
-    newParagraph.innerHTML = "<p>This is click number 1</p>";
-    document.getElementsByClassName( 'container' ) [0].appendChild( newElement);
-});
+//Now make something happen when I click the h1
+// newParagraph.addEventListener('click', myFunctionName () {
+//     var newElement = document.createElement( 'paragraph' );
+//     newParagraph.innerHTML = "<p>This is click number 1</p>";
+//     document.getElementsByClassName( 'container' ) [0].appendChild( newElement);
+// });
+newParagraph.addEventListener('click', myFunctionName);
+
+function myFunctionName() {
+    var paragraph = document.createElement('p');
+    paragraph.innerHTML = 'This is click number: ';
+    newParagraph.appendChild(paragraph);
+}
