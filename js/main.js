b198d3ea2bca1d89a981a8dfa52965cfcf57280b
Lori Gibbons <lori.j.gibbons@gmail.com>
2017-07-11 22:04:22 -0400

    Add numba var

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var newParagraph = document.getElementsByTagName('h1')[0]
+
 
 //Now make something happen when I click the h1
 // newParagraph.addEventListener('click', myFunctionName () {
@@ -6,10 +6,14 @@ var newParagraph = document.getElementsByTagName('h1')[0]
 //     newParagraph.innerHTML = "<p>This is click number 1</p>";
 //     document.getElementsByClassName( 'container' ) [0].appendChild( newElement);
 // });
+var numba = 0;
+
+var newParagraph = document.getElementsByTagName('h1')[0];
+
 newParagraph.addEventListener('click', myFunctionName);
 
 function myFunctionName() {
     var paragraph = document.createElement('p');
-    paragraph.innerHTML = 'This is click number: ';
+    paragraph.innerHTML = 'This is click number: ' + ++numba;
     document.body.appendChild(paragraph);
 }
