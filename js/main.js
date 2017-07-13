72fc03a70a2d25e2bcf937ddc68ddfc8b2b6a332
Lori Jane Gibbons <gibbons@uga.edu>
2017-07-13 16:32:24 -0400

    Update comments in JS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //First item
-//This changes the h1 color from  black to orange when moused over, and goes back to black when the mouse leaves the h1.
+//This changes the h1 color from  black to orange when moused over, and goes back to black when the mouse leaves the h1.  Listens for mouse over and mouse leave.
 var pageHeading = document.getElementById( 'heading' );
 pageHeading.addEventListener('mouseover', function(){
 	pageHeading.className =  'orange';
@@ -9,7 +9,7 @@ pageHeading.addEventListener('mouseleave', function(){
 });
 
 //Second item-
-//This adds text under the pictures when the first picture is clicked.
+//This adds text under the pictures when the first picture is clicked.  Listens for a click.
 //Resources: https://www.kirupa.com/html5/setting_css_styles_using_javascript.htm
 var addText = document.images[0];
 addText.addEventListener('click', function(){
@@ -19,7 +19,7 @@ addText.addEventListener('click', function(){
     newElement.style.clear = "both";
 });
 
-//Third item- Double click on second picture to delete text added in second item
+//Third item- Double click on second picture to delete text added in second item.  It listens for a double click.
 var removeText = document.images[1];
 removeText.addEventListener('dblclick', function(){
     var newElement = document.getElementsByTagName('h3');
@@ -30,7 +30,7 @@ removeText.addEventListener('dblclick', function(){
 });
 
 //Other item-
-//Adds random numbers to footer area when the mouse is moving. https://www.w3schools.com/js/js_htmldom_eventlistener.asp
+//Adds random numbers to footer area when the mouse is moving over text.  It listens for a mouse move. https://www.w3schools.com/js/js_htmldom_eventlistener.asp
 document.getElementById("numGen").addEventListener("mousemove", theFunction);
 function theFunction(){
     document.getElementById('numbers').innerHTML = Math.random();
