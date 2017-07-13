b2d46e6a0f996a99ad727ddb3f900c6cf1f1f0aa
Lori Jane Gibbons <gibbons@uga.edu>
2017-07-13 15:31:34 -0400

    Correct functions to add form to colsole log

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-var consoleAction = document.getElementById( 'reset' );
+//get the submit button
+var formSubmit = document.getElementById( 'submit' );
 
-consoleAction.addEventListener('click', function(thisFunction){
+//listen for submission of form while preventing submission of form
+formSubmit.addEventListener('click', function(thisFunction){
     thisFunction.preventDefault();
-    var fName = document.getElementById( 'firstName' );
-    console.log(fName);
+    var fName = document.getElementById('firstName').value;
+    console.log('The firstName is: ' + fName);
+    var lName = document.getElementById('lastName').value;
+    console.log('The lastName is: ' + lName);
+    var formEmail = document.getElementById('email').value;
+    console.log('The email is: ' + formEmail);
+    var formMessage = document.getElementById('message').value;
+    console.log('The message is: ' + formMessage);
 })
